572fc7e23dd088297458033eeb3133c6cb9797b4
Rahmat Hadinat <rsafei731@gmail.com>
2025-07-16 19:15:14 +0700

    todoapp

--- todoapp/tailwind.config.js
+++ todoapp/tailwind.config.js
@@ -3,6 +3,8 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class', // <- ini penting untuk mendukung toggle dark mode berbasis class
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
